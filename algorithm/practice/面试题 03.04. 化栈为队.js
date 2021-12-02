df77082afd6cae4243e1be223d2217aad45a4af5
上古 <1981914095@163.com>
2021-12-02 13:36:33 +0800

    Create 面试题 03.04. 化栈为队.js

---
+++ algorithm/practice/面试题 03.04. 化栈为队.js
@@ -0,0 +1,67 @@
+// https://leetcode-cn.com/problems/implement-queue-using-stacks-lcci/
+
+/**
+ * Initialize your data structure here.
+ */
+ var MyQueue = function() {
+    this.s1 = [] // 出栈
+    this.s2 = [] // 入栈
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.s1.push(x)
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.empty()) return false
+    if (this.s2.length) {
+        return this.s2.pop()
+    } else {
+        while(this.s1.length) {
+            this.s2.push(this.s1.pop())
+        }
+        return this.pop()
+    }
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if (this.empty()) return false
+    if (this.s2.length) {
+        return this.s2[this.s2.length - 1]
+    } else {
+        while(this.s1.length) {
+            this.s2.push(this.s1.pop())
+        }
+        return this.peek()
+    }
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return !(this.s1.length + this.s2.length)
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
