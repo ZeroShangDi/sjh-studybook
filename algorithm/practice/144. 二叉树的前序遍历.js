83db723bc4b5c4e14261df7d8ca378052266a674
上古 <1981914095@163.com>
2021-12-12 02:40:43 +0800

    Create 144. 二叉树的前序遍历.js

---
+++ algorithm/practice/144. 二叉树的前序遍历.js
@@ -0,0 +1,22 @@
+// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var preorderTraversal = function(root) {
+    if (!root) return []
+    return [
+        root.val,
+        ...preorderTraversal(root.left),
+        ...preorderTraversal(root.right),
+    ].filter(item => item)
+};
